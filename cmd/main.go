1e2b102f6059eeb221e67378408fea047307a5d1
John Heroy <john@fractal.is>
2023-07-19 14:55:03 -0700

    no env variables

--- cmd/main.go
+++ cmd/main.go
@@ -7,25 +7,37 @@ import (
 	"strings"
 
 	"github.com/fractalwagmi/fractal-cli/pkg/auth"
-	"github.com/fractalwagmi/fractal-cli/pkg/env"
 )
 
 func main() {
 	archive := flag.String("zip", "", "path to .zip archive of game binary")
+	clientId := flag.String("clientId", "", "Fractal client id")
+	clientSecret := flag.String("clientSecret", "", "Fractal client secret")
 
 	flag.Parse()
 
-	if !strings.HasSuffix(*archive, ".zip") {
-		log.Fatalf("Invalid archive: %s\n", *archive)
-	}
+	validateArguments(archive, clientId, clientSecret)
 
-	clientId := env.GetRequiredString("FRACTAL_CLIENT_ID")
-	clientSecret := env.GetRequiredString("FRACTAL_CLIENT_SECRET")
-
-	token, err := auth.GenerateToken(clientId, clientSecret)
+	token, err := auth.GenerateToken(*clientId, *clientSecret)
 	if err != nil {
 		log.Fatalf("Error generating token: %s\n", err)
 	}
 
 	fmt.Printf("Token: %s\n", token)
+}
+
+func validateArguments(
+	archive *string,
+	clientId *string,
+	clientSecret *string,
+) {
+	if !strings.HasSuffix(*archive, ".zip") {
+		log.Fatalf("Invalid archive: %s\n", *archive)
+	}
+	if len(*clientId) == 0 {
+		log.Fatalf("Invalid clientId: %s\n", *clientId)
+	}
+	if len(*clientSecret) == 0 {
+		log.Fatalf("Invalid clientSecret: %s\n", *clientSecret)
+	}
 }
