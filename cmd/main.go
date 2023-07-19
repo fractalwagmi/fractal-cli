566ad3bc526e31e107d779c1a62e75caaecfebd3
John Heroy <john@fractal.is>
2023-07-19 14:57:57 -0700

    args struct

--- cmd/main.go
+++ cmd/main.go
@@ -9,16 +9,24 @@ import (
 	"github.com/fractalwagmi/fractal-cli/pkg/auth"
 )
 
+type Args struct {
+	archive      string
+	clientId     string
+	clientSecret string
+}
+
 func main() {
-	archive := flag.String("zip", "", "path to .zip archive of game binary")
-	clientId := flag.String("clientId", "", "Fractal client id")
-	clientSecret := flag.String("clientSecret", "", "Fractal client secret")
+	args := Args{}
+
+	flag.StringVar(&args.archive, "zip", "", "path to .zip archive of game binary")
+	flag.StringVar(&args.clientId, "clientId", "", "Fractal client id")
+	flag.StringVar(&args.clientSecret, "clientSecret", "", "Fractal client secret")
 
 	flag.Parse()
 
-	validateArguments(archive, clientId, clientSecret)
+	validateArguments(args)
 
-	token, err := auth.GenerateToken(*clientId, *clientSecret)
+	token, err := auth.GenerateToken(args.clientId, args.clientSecret)
 	if err != nil {
 		log.Fatalf("Error generating token: %s\n", err)
 	}
@@ -26,18 +34,14 @@ func main() {
 	fmt.Printf("Token: %s\n", token)
 }
 
-func validateArguments(
-	archive *string,
-	clientId *string,
-	clientSecret *string,
-) {
-	if !strings.HasSuffix(*archive, ".zip") {
-		log.Fatalf("Invalid archive: %s\n", *archive)
+func validateArguments(args Args) {
+	if !strings.HasSuffix(args.archive, ".zip") {
+		log.Fatalf("Invalid archive: %s\n", args.archive)
 	}
-	if len(*clientId) == 0 {
-		log.Fatalf("Invalid clientId: %s\n", *clientId)
+	if len(args.clientId) == 0 {
+		log.Fatalf("Invalid clientId: %s\n", args.clientId)
 	}
-	if len(*clientSecret) == 0 {
-		log.Fatalf("Invalid clientSecret: %s\n", *clientSecret)
+	if len(args.clientSecret) == 0 {
+		log.Fatalf("Invalid clientSecret: %s\n", args.clientSecret)
 	}
 }
